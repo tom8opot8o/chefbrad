ff33fade68e562a667fa8fc763edad3efdaaf439
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2024-12-27 15:31:23 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const pageData = {
+    accountName: "chefbrad",
+    posts: [
+        {
+            image: "image1.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fcreamy-kimchi-tteokbokki%2F&aff_id=5295&offer_id=1&content_id=9037a9f8-eba5-4461-b7a3-95de0b83a78c&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+        },
+        {
+            image: "image2.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fpickled-carrots%2F&aff_id=5295&offer_id=1&content_id=2813d3f0-0ad0-4e98-a634-3c9e554b248f&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+        },
+        {
+            image: "image3.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fpickled-eggs%2F&aff_id=5295&offer_id=1&content_id=177849ae-c578-4e17-9771-cf9181dd9269&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+        }
+    ]
+};
