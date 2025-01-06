ec8de45c0d797f995baff2056d50aee269f04686
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2025-01-06 15:57:15 +0100

    Update data.js

--- data.js
+++ data.js
@@ -7,11 +7,11 @@ const pageData = {
         },
         {
             image: "image2.jpg",
-            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fpickled-carrots%2F&aff_id=5295&offer_id=1&content_id=2813d3f0-0ad0-4e98-a634-3c9e554b248f&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fcuban-tamales%2F&aff_id=5295&offer_id=1&content_id=fa3fe3bd-b72f-44ce-9fd3-2b29f6ca7ffe&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
         },
         {
             image: "image3.jpg",
-            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fpickled-eggs%2F&aff_id=5295&offer_id=1&content_id=177849ae-c578-4e17-9771-cf9181dd9269&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Froast-beef%2F&aff_id=5295&offer_id=1&content_id=7814cbe9-a52f-4dd5-918a-27e5f31372a4&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
         }
     ]
 };
